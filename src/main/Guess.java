143472655ca1e4b838f3b0e09fa3c8df879a34af
Rui <rui.bian@rea-group.com>
2015-06-11 19:31:26 +0800

    finish guess part

--- src/main/Guess.java
+++ src/main/Guess.java
@@ -1,5 +1,53 @@
+import java.util.*;
+
 public class Guess {
+
+    private int a = 0;
+    private int b = 0;
+    private HashMap<Integer, String> expectedMap = new HashMap<>();
+    private HashMap<Integer, String> actualMap = new HashMap<>();
+
     public String guess(String expect, String actual) {
-        return "4A0B";
+        format(expect, actual);
+
+        if (expectedMap.equals(actualMap)) {
+            return "4A0B";
+        }
+        List<Integer> correctIndex = new ArrayList<>();
+        getNumberOfA(correctIndex);
+        for (Integer integer : correctIndex) {
+            expectedMap.remove(integer);
+            actualMap.remove(integer);
+        }
+        getNumberOfB();
+        return String.format("%dA%dB", a, b);
+    }
+
+    private void format(String expect, String actual) {
+        List<String> expects = Arrays.asList(expect.split(""));
+        List<String> actuals = Arrays.asList(actual.split(""));
+        for (int i = 0; i < expects.size(); i++) {
+            expectedMap.put(i, expects.get(i));
+        }
+        for (int i = 0; i < actuals.size(); i++) {
+            actualMap.put(i, actuals.get(i));
+        }
+    }
+
+    private void getNumberOfB() {
+        for (Map.Entry<Integer, String> entry : actualMap.entrySet()) {
+            if (expectedMap.containsValue(entry.getValue())) {
+                b++;
+            }
+        }
+    }
+
+    private void getNumberOfA(List<Integer> correctIndex) {
+        for (Map.Entry<Integer, String> entry : actualMap.entrySet()) {
+            if (entry.getValue().equals(expectedMap.get(entry.getKey()))) {
+                a++;
+                correctIndex.add(entry.getKey());
+            }
+        }
     }
 }
--- src/test/GuessTest.java
+++ src/test/GuessTest.java
@@ -18,5 +18,18 @@ public class GuessTest {
         assertThat(guess.guess("1234", "1234"), is("4A0B"));
     }
 
-    
+    @Test
+    public void shouldReturn3A1BWhenOnlyOneNotAllMatch() throws Exception {
+        assertThat(guess.guess("1234", "1244"), is("3A0B"));
+    }
+
+    @Test
+    public void shouldReturn4B0AWhenAllOnlyOne() throws Exception {
+        assertThat(guess.guess("1234", "4321"), is("0A4B"));
+    }
+
+    @Test
+    public void shouldReturn0B0AWhenAllNotMatch() throws Exception {
+        assertThat(guess.guess("1234", "6789"), is("0A0B"));
+    }
 }
